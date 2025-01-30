9cea4464ef2894c5aac5ae54bf607b72a98fb0bf
Aman <156819993+amanrawat98@users.noreply.github.com>
2025-01-30 11:56:08 +0530

    Add update blog router with authentication and authorization middleware
    - Implemented verifyJwtToken middleware to verify JWT token.
    - Added authorizeRole middleware to allow only authors to update blogs.
    - Used upload.single to handle file uploads in the updateBlog route.
    - Integrated the updateBlog controller for updating a blog post.

--- src/routes/blogRouter.ts
+++ src/routes/blogRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createBlog, getAllBlogs, getBlogById, updateBlogStatus } from "../controllers/blog.controller";
+import { createBlog, getAllBlogs, getBlogById, updateBlog, updateBlogStatus } from "../controllers/blog.controller";
 import { verifyJwtToken } from "../utility/generateJwtToken";
 import { authorizeRole } from "../middleware/checkRole";
 import upload from "../config/multerConfig";
@@ -7,6 +7,7 @@ import upload from "../config/multerConfig";
 const router = express.Router();
 
 router.post("/blog", verifyJwtToken, authorizeRole("author"), upload.single('image'),createBlog);
+router.put("/blog/:id", verifyJwtToken, authorizeRole("author"), upload.single('image'),updateBlog);
 router.get("/blog", getAllBlogs);
 router.get("/blog/:id", getBlogById);
 router.put("/blog/:id/:status", verifyJwtToken, authorizeRole("admin"), updateBlogStatus);
